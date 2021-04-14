2b3975bfcdeaa89d0771bf446de76763e39c4b4b
Jérémy Roos <jeremy.roos@gmail.com>
2021-04-14 17:49:01 +0200

    coerce the elements of a gmm object to type double

--- NEWS.md
+++ NEWS.md
@@ -1,11 +1,11 @@
 ## gmgm (development version)
 
+* Modified function `gmm` to coerce the elements of the `gmm` object to type double.
 * Provided a more detailed example in the README file.
 
 ## gmgm 1.0.1
 
-* Removed a unit test of function `em` that failed with some BLAS/LAPACK
-implementations.
+* Removed a unit test of function `em` that failed with some BLAS/LAPACK implementations.
 
 ## gmgm 1.0.0
 
--- tests/testthat/test-gmm.R
+++ tests/testthat/test-gmm.R
@@ -6,6 +6,11 @@ gmm_1 <- list(alpha = c(0.4, 0.6),
                                   dimnames = list(c("A", "B"), c("A", "B")))))
 class(gmm_1) <- "gmm"
 
+gmm_2 <- list(alpha = 1, mu = matrix(0, 2, dimnames = list(c("A", "B"), NULL)),
+              sigma = list(matrix(c(1, 0, 0, 1), 2,
+                                  dimnames = list(c("A", "B"), c("A", "B")))))
+class(gmm_2) <- "gmm"
+
 test_that("create a gmm object", {
   expect_equal(gmm(alpha = c(0.4, 0.6),
                    mu = matrix(c(0, 2, 1, 3), 2,
@@ -13,6 +18,13 @@ test_that("create a gmm object", {
                    sigma = list(matrix(c(1, 1, 1, 2), 2),
                                 matrix(c(2, 3, 3, 5), 2))),
                gmm_1)
+})
+
+test_that("create a gmm object with one mixture component", {
+  expect_equal(gmm(alpha = 1,
+                   mu = matrix(0, 2, dimnames = list(c("A", "B"), NULL)),
+                   sigma = list(matrix(c(1, 0, 0, 1), 2))),
+               gmm_2)
 })
 
 test_that("create a gmm object whith non-normalized mixture proportions", {
@@ -25,7 +37,7 @@ test_that("create a gmm object whith non-normalized mixture proportions", {
 })
 
 test_that("create a gmm object with explicit variable names", {
-  expect_equal(gmm(alpha = c(0.8, 1.2),
+  expect_equal(gmm(alpha = c(comp_1 = 0.4, comp_2 = 0.6),
                    mu = matrix(c(0, 2, 1, 3), 2,
                                dimnames = list(c("C", "D"),
                                                c("comp_1", "comp_2"))),
